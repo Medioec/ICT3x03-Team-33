de0649d986aa5982b8e9c453b5d716c7fa53b74e
Ong Yi Xuan <2102623@sit.singaporetech.edu.sg>
2023-10-16 12:28:31 +0800

    integrate registration with db

--- cinema-booking/identity/app.py
+++ cinema-booking/identity/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from argon2 import PasswordHasher
 from email_validator import validate_email, EmailNotValidError
+import requests
 from flask_cors import CORS
+import user_utils
+
 app = Flask(__name__)
 CORS(app)
-
 
 # member registration
 @app.route("/register", methods=["POST"])
@@ -14,49 +16,67 @@ def register():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # check if username is available
-    response, status_code = isUsernameAvailable(username)
-
-    # if username is not in the database
-    if status_code == 200:
-        # validate email
-        try:
-            validate_email(email, check_deliverability=True)
-
-            # generate password hash
-            ph = PasswordHasher()
-            hash = ph.hash(password)
+    # ensure all fields are filled in
+    if (email is not None) and (username is not None) and (password is not None):
+        # if username is not in the database
+        if user_utils.isUsernameAvailable(username):
             
-            # get salt from hash
-            temp_arr = hash.split('$')
-            salt = temp_arr[:-2]
+            # validate password
+            if user_utils.validatePassword(password): 
 
-            # join remaining string together as hash
-            temp_arr = temp_arr[:-2] + temp_arr[-1:]
-            hash = '$'.join(temp_arr)
+                # validate email
+                try:
+                    validate_email(email, check_deliverability=True)
 
-            # insert email, username, hash, salt, and role into database
-            role = "member"
+                    # check if email is available
+                    if user_utils.isEmailAvailable(email):
 
-            return jsonify({"message": "Registration successful"}), 200
+                        # generate password hash
+                        ph = PasswordHasher()
+                        hash = ph.hash(password)
+
+                        salt = "blah"
+
+                        # generate userId and role
+                        role = "member"
+                        userId = user_utils.generateUUID()
+
+                        # insert email, username, hash and role into database
+                        data = {
+                            "userId": userId,
+                            "email": email,
+                            "username": username,
+                            "passwordHash": hash,
+                            "userRole": role
+                        }
+                        response = requests.post("http://databaseservice:8085/databaseservice/user/add_user", json=data)
+                        
+                        # if data inserted successfully into db, return HTTP ok
+                        if response.status_code == 201:
+                            return jsonify({"message": "Registration successful"}), 200
+                        
+                        # if data not inserted successfully into db, return HTTP internal server error
+                        else:
+                            return jsonify({"message": "Registration unsuccessful"}), 500
+                
+                    else:
+                        return jsonify({"message": "Email is already in use"}), 409
+                    
+                # if email is invalid, return HTTP bad request
+                except EmailNotValidError:
+                    return jsonify({"message": "Email is invalid"}), 400
+
+            else:
+                return jsonify({"message": "Password does not meet the requirements"}), 400
         
-        # if email is invalid
-        except EmailNotValidError:
-            return jsonify({"message": "Email is invalid"}), 400
-
-    # if username is taken
+        # if username is taken, return HTTP conflict 
+        else:
+            return jsonify({"message": "Username is already taken"}), 409 
+    
+    # if any field in form is incomplete, return error 
     else:
-        return jsonify({"message": response}), status_code 
-
-# for registrationL: check if username is available/does not exist in db
-def isUsernameAvailable(username): 
-    # check if username is taken
-    if username == "test": # placeholder: to replace with db service code
-        return "Username is already taken", 409
-
-    # if username does not exist
-    else:
-        return "Username is available", 200
+        return jsonify({"message": "Please fill in all form data"}), 400
+        
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=8081)
