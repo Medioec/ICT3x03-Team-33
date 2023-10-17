a45ec622597aa828c9b2c1b46831837c06ee3567
meloonboi <jkllim361@gmail.com>
2023-10-17 22:00:31 +0800

    Eops i deleted some stuff i shldnt have and put it back

--- cinema-booking/identity/app.py
+++ cinema-booking/identity/app.py
@@ -16,66 +16,45 @@ def register():
     username = request.form.get("username")
     password = request.form.get("password")
 
-    # ensure all fields are filled in
-    if (email is not None) and (username is not None) and (password is not None):
-        # if username is not in the database
-        if user_utils.isUsernameAvailable(username):
-            
-            # validate password
-            if user_utils.validatePassword(password): 
-
-                # validate email
-                try:
-                    validate_email(email, check_deliverability=True)
-
-                    # check if email is available
-                    if user_utils.isEmailAvailable(email):
-
-                        # generate password hash
-                        ph = PasswordHasher()
-                        hash = ph.hash(password)
-
-                        salt = "blah"
-
-                        # generate userId and role
-                        role = "member"
-                        userId = user_utils.generateUUID()
-
-                        # insert email, username, hash and role into database
-                        data = {
-                            "userId": userId,
-                            "email": email,
-                            "username": username,
-                            "passwordHash": hash,
-                            "userRole": role
-                        }
-                        response = requests.post("http://databaseservice:8085/databaseservice/user/add_user", json=data)
-                        
-                        # if data inserted successfully into db, return HTTP ok
-                        if response.status_code == 201:
-                            return jsonify({"message": "Registration successful"}), 200
-                        
-                        # if data not inserted successfully into db, return HTTP internal server error
-                        else:
-                            return jsonify({"message": "Registration unsuccessful"}), 500
-                
-                    else:
-                        return jsonify({"message": "Email is already in use"}), 409
-                    
-                # if email is invalid, return HTTP bad request
-                except EmailNotValidError:
-                    return jsonify({"message": "Email is invalid"}), 400
-
-            else:
-                return jsonify({"message": "Password does not meet the requirements"}), 400
-        
-        # if username is taken, return HTTP conflict 
-        else:
-            return jsonify({"message": "Username is already taken"}), 409 
-    
-    # if any field in form is incomplete, return error 
-    else:
+    if not email or not username or not password:
         return jsonify({"message": "Please fill in all form data"}), 400
+
+    if not user_utils.isUsernameAvailable(username):
+        return jsonify({"message": "Username is already taken"}), 409
+
+    if not user_utils.validatePassword(password):
+        return jsonify({"message": "Password does not meet the requirements"}), 400
+
+    try:
+        validate_email(email, check_deliverability=True)
+
+        if not user_utils.isEmailAvailable(email):
+            return jsonify({"message": "Email is already in use"}), 409
+
+        ph = PasswordHasher()
+        hash = ph.hash(password)
+
+        salt = "blah"
+        role = "member"
+        userId = user_utils.generateUUID()
+
+        data = {
+            "userId": userId,
+            "email": email,
+            "username": username,
+            "passwordHash": hash,
+            "userRole": role
+        }
+
+        response = requests.post("http://databaseservice:8085/databaseservice/user/add_user", json=data)
+
+        if response.status_code == 201:
+            return jsonify({"message": "Registration successful"}), 200
+        else:
+            return jsonify({"message": "Registration unsuccessful"}), 500
+
+    except EmailNotValidError:
+        return jsonify({"message": "Email is invalid"}), 400
         
     
 if __name__ == "__main__":
