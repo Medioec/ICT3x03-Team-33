0be368dbf465aad296a134bed13c14ef8dd8128d
Errick <91124693+Medioec@users.noreply.github.com>
2023-10-20 02:16:22 +0800

    Added Jenkinsfile

--- scripts/dev-deploy.sh
+++ scripts/dev-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run this script from base repository directory
 cp env/dev/.env .env
 docker compose -p cinema-dev up -d
 docker compose -p cinema-dev restart
--- scripts/prod-deploy.sh
+++ scripts/prod-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# run this script from base repository directory
 cp env/prod/.env .env
 docker compose -p cinema-prod up -d
 docker compose -p cinema-prod restart
