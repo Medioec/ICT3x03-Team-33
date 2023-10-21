f4bc2d78022a9ec3b75f8efb5f251443725350a9
meloonboi <jkllim361@gmail.com>
2023-10-21 12:40:07 +0800

    Made role not unique

--- cinema-booking/init.sql
+++ cinema-booking/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE CinemaUser (
     email VARCHAR(255) UNIQUE NOT NULL,
     username VARCHAR(255) UNIQUE NOT NULL,
     passwordHash VARCHAR(255) NOT NULL,
-    userRole VARCHAR(255) UNIQUE NOT NULL
+    userRole VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE UserSessions (
