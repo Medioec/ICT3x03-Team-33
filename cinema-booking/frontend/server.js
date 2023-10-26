d2bf9a2dd603d3c727ad476a512a0c864c7c988d
meloonboi <jkllim361@gmail.com>
2023-10-26 20:32:04 +0800

    Fixed function declared twice causing break

--- cinema-booking/frontend/server.js
+++ cinema-booking/frontend/server.js
@@ -40,14 +40,6 @@ app.use((req, res) => {
     res.status(404).send('Not Found');
 });
 
-//Headers checks, preventing content type attack/request forgery
-function checkHeaders(req, res, next) {
-    if (req.headers['content-type'] !== 'application/json' || req.headers['accept'] !== 'application/json') {
-        return res.status(400).send('Server expects application/json data');
-    }
-    next();
-}
-
 
 // ############################## IDENTITY SERVICE #########################################
 // handle GET request from login.html
