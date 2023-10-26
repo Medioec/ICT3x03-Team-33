116d59cb62830a47d09b7e28e61006fddee31e6a
2102596sit <2102596@sit.singaporetech.edu.sg>
2023-10-26 21:36:50 +0800

    removing duplicate headers checks through merges
    removing duplicate headers checks through merges

--- cinema-booking/frontend/server.js
+++ cinema-booking/frontend/server.js
@@ -41,12 +41,12 @@ app.use((req, res) => {
 });
 
 //Headers checks, preventing content type attack/request forgery
-function checkHeaders(req, res, next) {
-    if (req.headers['content-type'] !== 'application/json' || req.headers['accept'] !== 'application/json') {
-        return res.status(400).send('Server expects application/json data');
-    }
-    next();
-}
+// function checkHeaders(req, res, next) {
+    // if (req.headers['content-type'] !== 'application/json' || req.headers['accept'] !== 'application/json') {
+        // return res.status(400).send('Server expects application/json data');
+    // }
+    // next();
+// }
 
 
 // ############################## IDENTITY SERVICE #########################################
