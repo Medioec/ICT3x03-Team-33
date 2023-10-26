4b2b6baf0842113625d1db070ea1867cdc99ead8
Danial Hazeem <danialhbma@gmail.com>
2023-10-26 22:06:35 +0800

    Moviedetails dynamically loaded

--- cinema-booking/frontend/routes/homeRoutes.js
+++ cinema-booking/frontend/routes/homeRoutes.js
@@ -5,6 +5,9 @@ const checkHeaders = require('../middleware/checkHeaders');
 
 router.get('/', homeController.getHomePage);
 
+// route to moviedetails for a specific movie
+router.get('/moviedetails', homeController.getMovieDetailsPage);
+
 // Add other routes as needed
 
 module.exports = router;
