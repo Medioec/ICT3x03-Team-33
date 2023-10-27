2d44fe45351435dd4a820961a13f49a8958dbc3b
Ong Yi Xuan <2102623@sit.singaporetech.edu.sg>
2023-10-28 00:18:17 +0800

    add jwt handling in payment

--- cinema-booking/movie/app.py
+++ cinema-booking/movie/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from flask_jwt_extended import (JWTManager, jwt_required)
+from flask_jwt_extended import (JWTManager, jwt_required, get_jwt_identity)
 import os
 import requests
 import user_utils
