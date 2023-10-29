aba8392e902920fe3c0a0275a7b00fecc8fd66f7
Danial Hazeem <danialhbma@gmail.com>
2023-10-29 15:12:43 +0800

    Showtimes fully integrated
    - frontend working from main page all the way to seat selection
    - working on getting seat selection to insert into booking & payment

---
+++ cinema-booking/frontend/public/js/booking.js
@@ -0,0 +1,4 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
