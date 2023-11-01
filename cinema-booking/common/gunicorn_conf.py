408c2922096359894b65bdb66bad98ed484b4824
Errick <91124693+Medioec@users.noreply.github.com>
2023-11-01 23:23:37 +0800

    gunicorn setup for tls

--- cinema-booking/common/gunicorn_conf.py
+++ cinema-booking/common/gunicorn_conf.py
@@ -7,3 +7,15 @@ graceful_timeout = 120
 timeout = 120
 keepalive = 5
 threads = 3
+bind = "0.0.0.0:443"
+keyfile = "privkey.pem"
+certfile = "fullchain.pem"
+ca_certs = "serverca.crt"
+ssl_version = 2
+cert_reqs = 1
+preload = True
+ciphers = "ECDHE:!aNULL:!eNULL:!LOW:!3DES:!MD5:!EXP:!PSK:!SRP:!DSS:!RC4:!SHA"
+ssl_options = {
+    "ciphers": ciphers,
+    "server_side": True
+}
