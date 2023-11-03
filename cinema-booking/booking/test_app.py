d83fad557b41eae2cca74a386eb9a1fbdf333e0c
TerraRyu <94993911+TerraRyu@users.noreply.github.com>
2023-11-04 03:06:34 +0800

    Update test_app.py

--- cinema-booking/booking/test_app.py
+++ cinema-booking/booking/test_app.py
@@ -1,33 +1,60 @@
 # test_app.py
 import unittest
-from booking.app import app
+import json
+from app import app as flask_app
+from unittest.mock import patch, MagicMock
+from flask_jwt_extended import create_access_token, JWTManager
 
-class AppTestCase(unittest.TestCase):
+class FlaskAppTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.app = flask_app
+        cls.app.config['JWT_SECRET_KEY'] = 'mock_jwt_secret_key'
+        cls.app.config['TESTING'] = True
+        cls.jwt = JWTManager(cls.app)
 
     def setUp(self):
-        app.config['TESTING'] = True
-        self.app = app.test_client()
+        self.client = self.app.test_client()
+        with self.app.app_context():
+            self.access_token = create_access_token(identity='test_user')
 
-    def test_generate_booking(self):
-        # Add your test cases for /generateBooking endpoint
-        # Example test case
-        data = {
-            "userId": "some-user-id",
-            "creditCardId": 1,
-            "showtimeId": 1,
-            "seatId": 1,
-            "ticketPriceId": 1
-        }
-        response = self.app.post('/generateBooking', json=data)
-        self.assertEqual(response.status_code, 200)
+    def test_unauthorized_access(self):
+        response = self.client.post('/generateBooking')
+        self.assertEqual(response.status_code, 401, 'Unauthorized access not handled correctly')
 
-    def test_retrieve_one_booking(self):
-        response = self.app.get('/retrieveOneBooking/some-user-id/1')
-        self.assertEqual(response.status_code, 200)
+    @patch('app.session.post')
+    def test_generate_booking(self, mock_post):
+        # Prepare the mock responses
+        mock_responses = [
+            MagicMock(status_code=200, json=lambda: {"userId": "testUserId"}),
+            MagicMock(status_code=200, json=lambda: {"transactionId": "testTransaction"}),
+            MagicMock(status_code=201)
+        ]
+        mock_post.side_effect = mock_responses
 
-    def test_retrieve_all_bookings(self):
-        response = self.app.get('/retrieveAllBookings/some-user-id')
-        self.assertEqual(response.status_code, 200)
+        # Perform the request
+        response = self.client.post(
+            '/generateBooking',
+            headers={'Authorization': f'Bearer {self.access_token}'},
+            json={
+                'creditCardId': '12345',
+                'showtimeId': '67890',
+                'seatId': 'A1',
+                'ticketPriceId': '200'
+            }
+        )
+
+        # Check if user session retrieval is successful
+        self.assertEqual(mock_responses[0].status_code, 200, "User session retrieval failed")
+
+        # Check if payment service call is successful
+        self.assertEqual(mock_responses[1].status_code, 200, "Payment service call failed")
+
+        # Check if booking details creation is successful
+        self.assertEqual(mock_responses[2].status_code, 201, "Booking details creation failed")
+
+        # Check if the final response is as expected
+        self.assertEqual(response.status_code, 201, 'Booking generation did not succeed as expected')
 
 if __name__ == '__main__':
     unittest.main()
