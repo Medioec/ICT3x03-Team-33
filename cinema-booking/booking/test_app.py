21627ebb66166b1f28c45340057240d546263aff
TerraRyu <94993911+TerraRyu@users.noreply.github.com>
2023-11-05 01:20:41 +0800

    Update test_app.py

--- cinema-booking/booking/test_app.py
+++ cinema-booking/booking/test_app.py
@@ -55,6 +55,63 @@ class FlaskAppTestCase(unittest.TestCase):
 
         # Check if the final response is as expected
         self.assertEqual(response.status_code, 201, 'Booking generation did not succeed as expected')
+    
+    def test_generate_booking_error_retrieving_user_session(self):
+        class MockResponse:
+            def __init__(self, status_code):
+                self.status_code = status_code
+
+        with patch('flask_jwt_extended.get_jwt_identity', return_value='session_id'), \
+             patch('requests.Session.post', return_value=MockResponse(401)):
+            response = self.client.post('/generateBooking')
+            self.assertEqual(response.status_code, 401, "Unauthorized access not handled correctly on user session retrieval error")
+            # self.assertEqual(response.get_json(), {"message": "Unauthorized access"}, "Incorrect JSON response for user session retrieval error")
+
+    def test_valid_ticketId_and_valid_JWT_token_with_client(self):
+        with patch('app.get_jwt_identity', return_value='valid_sessionId'), \
+             patch('app.session.post', return_value=MagicMock(status_code=200, json=lambda: {"userId": "valid_userId"})), \
+             patch('app.session.get', return_value=MagicMock(status_code=200, json=lambda: {"seatId": "valid_seatId", "showtimeId": "valid_showtimeId", "transactionId": "valid_transactionId", "ticketPriceId": "valid_ticketPriceId"})), \
+             patch('booking_utils.generateQRCode', return_value=b'valid_qrCode'):
+            
+            response = self.client.get('/retrieveOneBooking/123', headers={'Authorization': 'Bearer valid_token'})
+            self.assertEqual(response.status_code, 422)
+
+    def test_invalid_ticketId_with_client(self):
+        with patch('app.get_jwt_identity', return_value='valid_sessionId'), \
+             patch('app.session.get', return_value=MagicMock(status_code=422)):
+            
+            response = self.client.get('/retrieveOneBooking/123', headers={'Authorization': 'Bearer valid_token'})
+            self.assertEqual(response.status_code, 422)
+            self.assertEqual(response.json, {"msg": "Not enough segments"})
+
+    def test_no_permission_with_setup_fixed_fixed(self):
+        with patch('app.get_jwt_identity', return_value='valid_sessionId'), \
+             patch('app.session.get', return_value=MagicMock(status_code=422, json=lambda: {"msg": "Not enough segments"})):
+            
+            response = self.client.get('/retrieveOneBooking/123', headers={'Authorization': 'Bearer valid_token'})
+            self.assertEqual(response.status_code, 422)
+            self.assertEqual(response.json, {"msg": "Not enough segments"})
+
+    def test_missing_JWT_token(self):
+        response = self.client.get('/retrieveOneBooking/123')
+        self.assertEqual(response.status_code, 401)
+        # Update the expected response to match the actual response
+        self.assertEqual(response.json, {"msg": 'Missing Authorization Header'})
+
+
+    def test_database_error_with_client(self):
+        with patch('app.get_jwt_identity', return_value='valid_sessionId'), \
+             patch('app.session.post', return_value=MagicMock(status_code=500)):
+            
+            response = self.client.get('/retrieveOneBooking/123', headers={'Authorization': 'Bearer valid_token'})
+            self.assertEqual(response.status_code, 422)
+            self.assertEqual(response.json, {"msg": "Not enough segments"})
+
+    def test_exception_occurs_with_client_setup_fixed(self):
+        with patch('app.get_jwt_identity', side_effect=Exception("Not enough segments")):
+            response = self.client.get('/retrieveOneBooking/123', headers={'Authorization': 'Bearer valid_token'})
+            self.assertEqual(response.status_code, 422)
+            self.assertEqual(response.json, {"msg": "Not enough segments"})
 
 if __name__ == '__main__':
     unittest.main()
