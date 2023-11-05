ada609fd0fcafec1372d45d878fbdf13750e19b1
TerraRyu <94993911+TerraRyu@users.noreply.github.com>
2023-11-05 22:35:14 +0800

    update

--- cinema-booking/testing.py
+++ cinema-booking/testing.py
@@ -1,53 +1,41 @@
 import unittest
 import os
+import sys
 
-def run_tests_in_directory(directory, loader, suite, exclusions):
-    """Run tests in the given directory, excluding the ones specified."""
-    print(f"Running tests in: {directory}")
-    for dirpath, dirnames, filenames in os.walk(directory, topdown=True):
-        # Skip excluded directories
-        dirnames[:] = [d for d in dirnames if os.path.abspath(os.path.join(dirpath, d)) not in exclusions]
+def discover_tests(start_dir, exclude_dirs):
+    tests = []
+    for dirpath, dirnames, filenames in os.walk(start_dir):
+        for filename in filenames:
+            if filename.endswith('.py') and filename != '__init__.py' and filename.startswith('test_'):
+                test_path = os.path.join(dirpath, filename)
+                if not any(exclude_dir in test_path for exclude_dir in exclude_dirs):
+                    print(f"Discovered test file: {test_path}")
+                    tests.append(test_path)
+    return tests
 
-        # Perform test discovery in this directory
-        tests = loader.discover(start_dir=dirpath, pattern='test*.py')
-        suite.addTests(tests)
 
-def run_all_tests(start_dir, exclusions=None):
-    if exclusions is None:
-        exclusions = []
+if __name__ == '__main__':
+    tests = discover_tests('booking', ['database', 'databaseservice', 'frontend', 'identity'])
+
+    sys.path.append('cinema-booking')
 
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
 
-    # Normalize paths to avoid issues with OS-specific path formats
-    exclusions = [os.path.abspath(exclusion) for exclusion in exclusions]
+    # Load and add test cases from each discovered test file
+    for test in tests:
+        print(f"Loading test module: {test}")
+        try:
+            module = loader.loadTestsFromModule(test)
+            print(f"Loaded test module: {module}")
 
-    # Iterate through each subdirectory and run tests
-    for item in os.listdir(start_dir):
-        dirpath = os.path.join(start_dir, item)
-        if os.path.isdir(dirpath) and dirpath not in exclusions:
-            run_tests_in_directory(dirpath, loader, suite, exclusions)
+            for test_case in module:
+                suite.addTest(test_case)
 
-    if suite.countTestCases() == 0:
-        print("No tests found. Check that your test files are correctly named and located.")
-        return False
+        except ImportError as e:
+            print(f"Error importing test module {test}: {e}")
+        except Exception as e:
+            print(f"Error loading test module {test}: {e}")
 
-    print("Running tests...")
-    runner = unittest.TextTestRunner(verbosity=2)
-    result = runner.run(suite)
-
-    return result.wasSuccessful()
-
-if __name__ == '__main__':
-    test_dir = os.path.abspath('cinema-booking')
-    exclusions = [
-        os.path.join(test_dir, 'database'),
-        os.path.join(test_dir, 'databaseservice'),
-        os.path.join(test_dir, 'frontend'),
-        os.path.join(test_dir, 'identity'),
-    ]
-
-    print("Exclusion directories:", exclusions)
-    success = run_all_tests(test_dir, exclusions=exclusions)
-    exit_code = 0 if success else 1
-    exit(exit_code)
+    runner = unittest.TextTestRunner()
+    runner.run(suite)
